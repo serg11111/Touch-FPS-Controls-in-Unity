c82909dfb136072a8e0f52a1d082cfc5e8c3a1ae
serg11111 <vardanarsb@gmail.com>
2019-12-10 18:07:31 +0200

    Move Android First Person
    Move Android First Person Mine craft

---
+++ Assets/MyScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+public class MyScript : MonoBehaviour
+{
+
+    public FixedJoystick MoveJoystick;
+    public FixedButton JumpButton;
+    public FixedTouchField TouchField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var fps = GetComponent<RigidbodyFirstPersonController>();
+
+        fps.RunAxis = MoveJoystick.input;
+        fps.JumpAxis = JumpButton.Pressed;
+
+        fps.mouseLook.LookAxis = TouchField.TouchDist;
+    }
+}
